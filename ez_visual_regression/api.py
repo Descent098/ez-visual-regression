254a75a4bc914fae7fd2b7231c004e3342aa9cf7
Kieran Wood <kieran@canadiancoding.ca>
2023-09-28 08:40:52 -0600

    Added assert_image_similarity_to_baseline() and removed contributing.md

--- ez_visual_regression/api.py
+++ ez_visual_regression/api.py
@@ -4,24 +4,32 @@ import time                                                            # Allows 
 import logging                                                         # Enables logging
 from typing import Union
 
+# Third Party Dependencies
+## Image comparison
+from ez_img_diff.api import compare_images
+
 ## Browser automation
 from selenium import webdriver                                         # Instantiates a browser
 from selenium.webdriver.common.by import By                            # Specify find_element type
 from selenium.common.exceptions import NoSuchElementException          # Allows for error catching
 
-def get_screenshot(driver:webdriver.Chrome, url:str, filename:str, locator:Union[str, None]=None, by:By=By.ID):
+def get_screenshot(driver:webdriver, url:str, filename:str, locator:Union[str, None]=None, by:By=By.ID):
     """Takes a screenshot of a page or element
 
     Parameters
     ----------
     driver : webdriver
         The browser to use for capturing screenshots
+
     url : str
         The URl you want to get a screenshot from (or filepath)
+
     filename : str, optional
         The file to export the screenshot to, by default None
+
     locator : Union[str, None], optional
         The locator to search with (i.e. element ID, class, xpath etc.), by default None
+
     by : By, optional
         Define how to use the locator to find an element, by default By.ID
 
@@ -69,3 +77,51 @@ def get_screenshot(driver:webdriver.Chrome, url:str, filename:str, locator:Union
                 exit(1)
     else: # Screenshot page
         driver.save_screenshot(filename)
+
+def assert_image_similarity_to_baseline(driver:webdriver, url:str, folder:str, locator:Union[str, None]=None, by:By=By.ID, warning_threshold:float=10, error_threshold:float=30):
+    """Asserts the current screenshot of a page is similar to `<folder>/baseline.png` within: 0 < diff < error_threshold
+
+    Parameters
+    ----------
+    driver : webdriver
+        The browser to use for capturing screenshots
+
+    url : str
+        The URl you want to get a screenshot from (or filepath)
+
+    folder : str
+        The folder to save the baseline, threshold, current and difference images to
+
+    locator : Union[str, None], optional
+        The locator to search with (i.e. element ID, class, xpath etc.), by default None
+
+    by : By, optional
+        Define how to use the locator to find an element, by default By.ID
+
+    warning_threshold : float, optional
+        The threshold at which there will be a logged warning, by default 10
+
+    error_threshold : float, optional
+        The threshold at which an explicit error will be thrown, by default 30
+
+    Raises
+    ------
+    AssertionError
+        If diff > error_threshold
+    """
+    if not os.path.isdir(folder):
+        print(f"No directory was found called {folder}, creating...")
+        os.mkdir(folder)
+    if not os.path.exists(os.path.join(folder, "baseline.png")):
+        print(f"No baseline image was found called {os.path.join(folder, 'baseline.png')}, creating...")
+        get_screenshot(driver, url, os.path.join(folder, "baseline.png"), locator, by)
+
+    get_screenshot(driver, url, os.path.join(folder, "current.png"), locator, by)
+
+    diff = compare_images(os.path.join(folder, "baseline.png"), os.path.join(folder, "current.png"), os.path.join(folder, "diff.png"), os.path.join(folder, "thresh.png"))
+
+    if diff > error_threshold:
+        logging.error(f"Difference {diff} is over error threshold {error_threshold}")
+        raise AssertionError(f"Difference {diff} is over error threshold {error_threshold}")
+    if error_threshold > diff > warning_threshold:
+        logging.warning(f"Difference {diff} is over warning threshold {error_threshold}")
